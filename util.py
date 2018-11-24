dbfff492686b91fc393b919c89bff700646007ee
mkanakis <marioskanakis@gmail.com>
2018-11-24 16:11:49 +0100

    Update - add typing convention for hash_id function

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ import hashlib
 # Simple SHA-1 hash function as mentioned in CHORD protocol paper
 # @Parameters: id
 # @Returns: hashed id
-def hash_id(id):
+def hash_id(id: str) -> str:
     sha1 = hashlib.sha1()
     sha1.update(("%s" % (id)).encode("utf-8"))
     return sha1.hexdigest()
