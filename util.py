a4d9d04d86dc5a855663f5ffb3b5235ca54ac05b
mkanakis <marioskanakis@gmail.com>
2018-11-24 15:24:13 +0100

    Update - Removed unused functions from util.py Added - util_test.py for unittesting the hash function

--- util.py
+++ util.py
@@ -9,24 +9,3 @@ def hash_ip(ip):
     sha1.update(('%s' % (ip)).encode('utf-8'))
     hashedip = sha1.hexdigest()
     return hashedip
-
-
-# Test SHA-1 hash function
-def hash_test():
-    # Test
-    print (hash_ip('marios'))
-    print (hash_ip('marta'))
-    print (hash_ip('tasos'))
-    print (hash_ip('davide'))
-
-# Send Message
-def send_msg(msg):
-    return
-
-# Return Message
-def receive_msg():
-    return
-
-
-
-
