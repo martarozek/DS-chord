8c17677fdcd66b0e27a816a7a13363b1695be37f
mkanakis <marioskanakis@gmail.com>
2018-11-26 20:25:34 +0100

    Update - Add more complex template for init and join

--- node.py
+++ node.py
@@ -9,14 +9,28 @@ class Node:
     a node in our system
     """
 
-    def __init__(self, ip: str, port: int) -> None:
-        self.ip = ip
+    def __init__(self, address: str, port: int, remote_address: str = None) -> None:
+        self.address = address
         self.port = port
+
+        # Set successor / predecessor
+        # self.successor = None
+        # self.predecessor = None
+
+        # The Node upon initialization should seek 
+        # to join a ring or create one
+        self.join(remote_address)
 
     def look_up(self, key: str) -> None:
         return
 
-    def join(self, n: Node) -> None:
+    def join(self, remote_address: str) -> None:
+        
+        if remote_address:
+            return
+        else:
+            return
+
         return
 
     def create(self) -> None:
@@ -36,7 +50,7 @@ class Node:
 
 def run_server() -> None:
     n = Node("localhost", 8000)
-    server = SimpleXMLRPCServer((n.ip, n.port))
+    server = SimpleXMLRPCServer((n.address, n.port))
     server.register_instance(n)
 
     print("Serving XML-RPC on localhost port 8000")
