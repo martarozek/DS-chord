fae7d97d5dbd4863a67e95b925bfa8f3c49f3857
mkanakis <marioskanakis@gmail.com>
2018-11-28 01:12:12 +0100

    Add configurations.py for settings and Update node.py to generate unique ID by hashing -> modulo operation

---
+++ configurations.py
@@ -0,0 +1,8 @@
+# CONFIGURATIONS FILE
+# This file serves as an internal configurations file
+
+# --Variables will be declared, people will not be happy--
+
+# Initial size of ring 64
+LOGSIZE = 5
+SIZE = 1 << LOGSIZE
--- node.py
+++ node.py
@@ -1,7 +1,9 @@
+import sys
 import xmlrpc.client
 from xmlrpc.server import SimpleXMLRPCServer
-import util
-import sys
+
+from util import *
+from configurations import *
 
 
 class Node:
@@ -12,7 +14,8 @@ class Node:
     def __init__(self, address: str, port: int, remote_address: str = None) -> None:
         self.address = address
         self.port = port
-
+        self.id = self.generate_id("%s:%s" % (self.address, str(self.port)))
+        
         # Set successor / predecessor
         # self.successor = None
         # self.predecessor = None
@@ -20,6 +23,9 @@ class Node:
         # The Node upon initialization should seek
         # to join a ring or create one
         self.join(remote_address)
+
+    def generate_id(self, value: str) -> str:
+        return int(hash_id(value), 16) % SIZE
 
     def look_up(self, key: str) -> None:
         return
@@ -33,7 +39,6 @@ class Node:
             proxy = xmlrpc.client.ServerProxy(remote_address)
             # ring_address = proxy.request_join(self.address, self.port) not implemented yet
             ring_address = "http://localhost:8000/"
-
             if ring_address:
                 print(proxy.add(3, 2))
             else:
