dbfa57d39ff2de82c0ac3365b87c995804754a49
Marta Rożek <marta.rozek@gmail.com>
2018-12-01 21:51:39 +0100

    Polish the stdin file.

--- stdin.py
+++ stdin.py
@@ -4,23 +4,18 @@ from util import get_url
 from config import APP_PORT, APP_IP
 
 
-def user_input(app):
-    get_or_put = ""
+if __name__ == "__main__":
+    app = ServerProxy(get_url(APP_IP, APP_PORT))
     while True:
         get_or_put = input("Get(g) or put(p)?: ").lower()
         if get_or_put == "exit":
             break
         if get_or_put == "g":
             key = input("What is the key? ")
-            print(type(key))
-            return app.get(key)
+            print(app.get(key))
         elif get_or_put == "p":
             key = input("What is the key? ")
             value = input("What is the value? ")
-            return app.put(key, value)
+            print(app.put(key, value))
         else:
             print("Option not available. Try again.")
-
-if __name__ == '__main__':
-    app = ServerProxy(get_url(APP_IP, APP_PORT))
-    user_input(app)
