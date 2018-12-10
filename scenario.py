43d7404f418b64b77e0c210843bf95ccb05e64c7
mkanakis <marioskanakis@gmail.com>
2018-12-10 16:38:13 +0100

    Merged Scenario with master

--- scenario.py
+++ scenario.py
@@ -1,32 +1,31 @@
+import argparse
+import random, string
 from xmlrpc.client import ServerProxy
-
 from util import get_url
 from config import APP_PORT, APP_IP
 
-
-def scenario_1(app):
-    app.put("3", "3")
-    app.put("2", "2")
-    app.put("1", "1")
-
-
-def scenario_2(app):
-    app.get("3")
-    app.get("2")
-    app.get("1")
+# function for create a random value
+def randomword():
+    letters = string.ascii_lowercase
+    return "".join(random.choice(letters) for i in range(10))
 
 
-def scenario_3(app):
-    app.put("4", "4")
-    app.get("4")
-    app.put("5", "5")
-    app.get("5")
-    app.put("6", "6")
-    app.get("6")
+def scenario(app, get_num: int, put_num: int) -> None:
+    for i in range(put_num):
+        app.put(i, randomword())
+    for i in range(get_num):
+        app.get(i)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Start new test setting the scenario.")
+    parser.add_argument(
+        "--get", type=int, default=0, required=False, help="number of the get calls"
+    )
+    parser.add_argument(
+        "--put", type=int, default=0, required=False, help="number of put calls"
+    )
+
+    args = parser.parse_args()
     app = ServerProxy(get_url(APP_IP, APP_PORT))
-    scenario_1(app)
-    scenario_2(app)
-    scenario_3(app)
+    scenario(app, args.get, args.put)
